ed1eb9d0efde52b66175151735e0e73efd134098
MarioA96 <marioaquirozs@gmail.com>
2024-03-21 02:23:29 -0600

    Se agregaron archivos
    Se agrego la lógica para la autenticación en firebase desde la interfaz de login.

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,15 +1,48 @@
+import { useMemo } from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+
 import { Link as RouterLink } from "react-router-dom";
 import { Google } from "@mui/icons-material";
-import { Link, Button, Grid, Typography, TextField } from "@mui/material";
+import { Link, Button, Grid, Typography, TextField, Alert } from "@mui/material";
 import { AuthLayout } from "../layout/AuthLayout";
+
+import { useForm } from "../../hooks";
+import { startGoogleSignIn, startLoginWithEmailPassword } from "../../store/auth";
 
 
 export const LoginPage = () => {
+
+    const { status, errorMessage } = useSelector( state => state.auth );
+
+    const dispatch = useDispatch();
+
+    const { email, password, onInputChange } = useForm({
+        email: '',
+        password: ''
+    });
+
+    const isAuthenticating = useMemo( () => status === 'checking', [ status ] );
+
+    const onSubmit = ( event ) => {
+
+        event.preventDefault();
+
+        console.log({ email, password })
+        dispatch( startLoginWithEmailPassword({ email, password }) );
+    }
+
+    const onGoogleSignIn = () => {
+        
+        console.log('On Google Sign In');
+        dispatch( startGoogleSignIn() );
+    }
+
     return (
         
         <AuthLayout title="Iniciar sesión">
 
-            <form>
+            <form onSubmit={ onSubmit } className='animate__animated animate__fadeIn animate__faster'>
 
                 <Grid container>
                     <Grid item xs={ 12 } sx={{ mt: 2 }}>
@@ -18,6 +51,9 @@ export const LoginPage = () => {
                             type="email" 
                             placeholder="correo@dominio.com"
                             fullWidth
+                            name="email"
+                            value={ email }
+                            onChange={ onInputChange }
                         />
                     </Grid>
 
@@ -27,18 +63,44 @@ export const LoginPage = () => {
                             type="password" 
                             placeholder="Contraseña"
                             fullWidth
+                            name="password"
+                            value={ password }
+                            onChange={ onInputChange }
                         />
+                    </Grid>
+                </Grid>
+
+                <Grid 
+                    container
+                    display={ !!errorMessage ? '' : 'none'}
+                    sx={{ mt: 1}}
+                >
+                    <Grid 
+                        item 
+                        xs={ 12 }
+                    >
+                        <Alert severity='error'>{ errorMessage }</Alert>
                     </Grid>
                 </Grid>
 
                 <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
                     <Grid item xs={ 12 } sm={ 6 }>
-                        <Button variant="contained" fullWidth>
+                        <Button 
+                            disabled={ isAuthenticating }
+                            type="submit" 
+                            variant="contained" 
+                            fullWidth
+                        >
                             Login
                         </Button>
                     </Grid>
                     <Grid item xs={ 12 } sm={ 6 }>
-                        <Button variant="contained" fullWidth>
+                        <Button 
+                            disabled={ isAuthenticating }
+                            variant="contained" 
+                            fullWidth
+                            onClick={ onGoogleSignIn }
+                        >
                             <Google />
                             <Typography sx={{ ml: 1 }}>
                                 Google
